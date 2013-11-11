0dd75865747563f5d49877bdfbae4ebca3096cec
Krzysztof Sakrejda-Leavitt <krzysztof.sakrejda@gmail.com>
2013-11-11 12:30:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from distutils.core import setup
 setup(name='Resourcery',
       version='0.0.1',
       description='Data archiving',
-      author='Krzysztof Sakrejda-Leavitt',
-      author_email='krzysztof.sakrejda@gmail.com'#,
-      #packages=['resourcer']
+      author='Krzysztof Sakrejda'
      )
 
